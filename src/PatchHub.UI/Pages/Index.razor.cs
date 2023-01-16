0e2f15e3e901c279f1d26366d1e53c21340edfef
Max <maxcullifer@gmail.com>
2023-01-16 17:41:53 -0600

    feat: add new home page layout
    Add new home page layout. Add under construction header and icon for Recent and Favorited tabs.

--- src/PatchHub.UI/Pages/Index.razor.cs
+++ src/PatchHub.UI/Pages/Index.razor.cs
@@ -7,26 +7,62 @@ namespace PatchHub.UI.Pages;
 
 public partial class Index
 {
-    [Inject]
-    private SteamApiService SteamApi { get; set; } = default!;
+	[Inject]
+	private SteamApiService SteamApi { get; set; } = default!;
 
-    private List<SteamAppPopular> _popularApps = new();
+	private List<SteamAppPopular> _popularApps = new();
 
-    private MudListItem? selectedRecentItem;
+	private MudListItem? selectedRecentItem;
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            await foreach (var popularApp in SteamApi.GetMostPopularAsync(14))
-            {
-                if (!string.IsNullOrEmpty(popularApp.AppName))
-                {
-                    _popularApps.Add(popularApp);
-                    StateHasChanged();
-                }
-            }
-        }
-        await base.OnAfterRenderAsync(firstRender);
-    }
+	private bool hideTopGames = false;
+
+	private bool hideRecent = true;
+
+	private bool hideFavorited = true;
+
+	protected override async Task OnAfterRenderAsync(bool firstRender)
+	{
+		if (firstRender)
+		{
+			await foreach (var popularApp in SteamApi.GetMostPopularAsync(14))
+			{
+				if (!string.IsNullOrEmpty(popularApp.AppName))
+				{
+					_popularApps.Add(popularApp);
+					StateHasChanged();
+				}
+			}
+		}
+		await base.OnAfterRenderAsync(firstRender);
+	}
+
+	private void ShowContent(ContentType contentType)
+	{
+		if (contentType == ContentType.TopGames)
+		{
+			hideTopGames = false;
+			hideRecent = true;
+			hideFavorited = true;
+		}
+		else if (contentType == ContentType.Recent)
+		{
+			hideTopGames = true;
+			hideRecent = false;
+			hideFavorited = true;
+		}
+		else if (contentType == ContentType.Favorited)
+		{
+			hideTopGames = true;
+			hideRecent = true;
+			hideFavorited = false;
+		}
+		StateHasChanged();
+	}
+
+	private enum ContentType
+	{
+		TopGames,
+		Recent,
+		Favorited
+	}
 }
