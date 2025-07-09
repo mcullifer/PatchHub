da2f9f799a23fa0c3562edec824c30ce57bb3981
Max <maxcullifer@gmail.com>
2025-07-09 14:10:09 -0500

    fix: some bb code parsing issues

--- src/PatchHub.Web/src/lib/services/BBCodeService.ts
+++ src/PatchHub.Web/src/lib/services/BBCodeService.ts
@@ -8,6 +8,7 @@ export class BBCodeService {
 		for (const bbTag in BBToHTML) {
 			html = html.replaceAll(bbTag, BBToHTML[bbTag]);
 		}
+		html = html.replaceAll('{STEAM_CLAN_IMAGE}', this.steamClanImgUrl);
 
 		// Handle special cases like [url=...]...[/url]
 		// it also needs to handle the case when it's just [url]...[/url]
@@ -17,16 +18,16 @@ export class BBCodeService {
 		// Handle list items and ensure closing </li> tags
 		html = html.replace(/\[\*\](.*?)($|\[\*\]|\[\/list\])/g, '<li>$1</li>$2');
 
-		// Handle [previewyoutube=...]...[/previewyoutube] with a preview
+		// /\[previewyoutube=(.*?);.*?\](.*?)\[\/previewyoutube\]/g,
 		html = html.replace(
-			/\[previewyoutube=(.*?);.*?\](.*?)\[\/previewyoutube\]/g,
+			/\[previewyoutube=(?:"?)([^";\]\s]+).*?\](.*?)\[\/previewyoutube\]/g,
 			(match, videoId) => {
 				const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
 				const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
 				return `
 				<div>
 					<a href="${videoUrl}" target="_blank">
-						<img src="${thumbnailUrl}" alt="YouTube Video Thumbnail" style="margin-left: auto;margin-right:auto;">
+						<img src="${thumbnailUrl}" alt="YouTube Video Thumbnail" style="margin-left: auto;margin-right:auto;" />
 					</a>
 				</div>`;
 			}
@@ -36,7 +37,12 @@ export class BBCodeService {
 		// Example: [color=#FF474D]Old[/color] -> <span style="color:#FF474D">Old</span>
 		html = html.replace(/\[color=(.*?)\](.*?)\[\/color\]/g, '<span style="color:$1">$2</span>');
 
-		html = html.replaceAll('{STEAM_CLAN_IMAGE}', this.steamClanImgUrl);
+		// Handle [img src="..."][/img], [img src=...][/img], and [img]...[/img] tags
+		html = html.replace(
+			/\[img\s+src=(?:"(.*?)"|([^\]\s]+))\]\[\/img\]/g,
+			(_, src1, src2) => `\n<img src="${src1 || src2}" />\n`
+		);
+		html = html.replace(/\[img\](.*?)\[\/img\]/g, `\n<img src="$1" />\n`);
 		return html;
 	}
 }
